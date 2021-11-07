98f7c9f4b088281603a2fec7306b48ebf1ab4340
dtnfkmdtnfkm3@mail.ru <Clarchik>
2021-11-07 15:53:11 +0200

    Test spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,6 +23,6 @@ describe('AppComponent', () => {
     it(`should have as title 'SoftServeEducation'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('SoftServeEducation');
+        expect(app.title).toEqual('SoftServeEducations');
     });
 });
