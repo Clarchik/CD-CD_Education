58d40bd2077fe4551ad1ff700e5006c414cd5a88
dtnfkmdtnfkm3@mail.ru <Clarchik>
2021-11-07 15:09:02 +0200

    Inheritance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'SoftServeEducation';
+    title = 'SoftServeEducation';
+
+    constructor() {
+        const dog = new Dog('Olly', 'Male');
+    }
 }
---
+++ src/app/classes/animal.ts
@@ -0,0 +1,34 @@
+interface AnimalI {
+    name: string;
+    gender: string;
+    voice: () => void;
+}
+
+abstract class Animal implements AnimalI {
+    public get gender(): string {
+        return this._gender;
+    }
+    public set gender(value: string) {
+        this._gender = value;
+    }
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+    constructor(private _name: string, private _gender: string) {
+        this._name = _name;
+        this._gender = _gender;
+    }
+
+    public voice(): void {
+        console.log('VOICE');
+    }
+}
+
+class Dog extends Animal {
+    constructor(private dogName: string, private dogDenger: string) {
+        super(dogName, dogDenger);
+    }
+}
